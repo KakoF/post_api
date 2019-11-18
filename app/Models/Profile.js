4e283bf7133a53840e51fdbf37154224aff9d584
marcosferrare <marcosferrare@DTI02>
2019-11-18 16:37:09 -0300

    Tentativa para tratar o perfil

--- app/Models/Profile.js
+++ app/Models/Profile.js
@@ -5,6 +5,11 @@ const Model = use('Model')
 
 class Profile extends Model {
   
+  //TENTAR RETIRAR O INCREMENTO DA MIGRATION E UTILIZAR A LÓGICA JÁ EXISTENTE PARA GERENCIAR O PERFIL
+  /*static get primaryKey () {
+    return 'user_id'
+  }*/
+  
   users () {
     return this.belongsTo('App/Models/User')
   }
